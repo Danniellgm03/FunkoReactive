d592fa7114e052723f00c61f70eecccbfa44883d
daniel <daniell.gm03@gmail.com>
2023-10-10 01:21:07 +0200

    ci(dependencies): Añadimos dependencias

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,17 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("io.projectreactor:reactor-core:3.5.10")
+
+    implementation("io.r2dbc:r2dbc-h2:1.0.0.RELEASE")
+    implementation("io.r2dbc:r2dbc-pool:1.0.0.RELEASE")
+
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+
+    testImplementation("org.mockito:mockito-junit-jupiter:5.5.0")
+    testImplementation("org.mockito:mockito-core:5.5.0")
+
 }
 
 tasks.test {
