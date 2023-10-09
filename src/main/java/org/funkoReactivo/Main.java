88a8c3a903576156af8c842de7df3dda9dbd79f8
daniel <daniell.gm03@gmail.com>
2023-10-10 01:20:40 +0200

    feat(main): Main ejecutar csvManager

--- src/main/java/org/funkoReactivo/Main.java
+++ src/main/java/org/funkoReactivo/Main.java
@@ -1,7 +1,19 @@
 package org.funkoReactivo;
 
+import org.funkoReactivo.services.files.CsvManager;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String path = Paths.get("").toAbsolutePath() + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "data";
+        String file_path = path + File.separator + "funkos.csv";
+
+        CsvManager csvManager = new CsvManager();
+        csvManager.readCsv(file_path).subscribe(
+                funko -> System.out.println(funko)
+        );
     }
 }
